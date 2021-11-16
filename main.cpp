0ebb5c60821a84b64efdcb24d566de74ef780bc7
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2021-11-16 16:11:05 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -74,14 +74,6 @@ int main()
     //LCD Backlight ON
     backLight = 1;
 
-    //Mean Gyro Values
-    Motion_t gyrMean   = motion.getGyro();
-    bool isFirstRun = true;
-
-    //Integrated Gyro Values
-    Motion_t gyrAngle = {.x=0.0, .y=0.0, .z = 0.0};
-    Motion_t gyrPrev  = {.x=0.0, .y=0.0, .z = 0.0};    
-
     // Initial display
     disp.cls();
     disp.locate(0,0);
@@ -90,89 +82,21 @@ int main()
     disp.printf("%.2f %.1f", env.getTemperature(), env.getPressure());
     wait_us(200000); 
 
-    // Timer
-    Timer tmr;
-    tmr.start();
-    std::chrono::microseconds now = 0us;
-    std::chrono::microseconds prev = 0us;
 
 
     while (true) {
         
-        //On first execution, calibrate the gyros
-        if (isFirstRun) {
-            printf("CALIBRATING\n\r");
-            led1 = 1; led2 = 1; led3 = 1;
-            gyrMean  = {0.0, 0.0, 0.0};
-            gyrAngle = gyrMean;
-            gyrPrev  = gyrMean;
-
-            for (uint32_t n=0; n<100; n++) {
-                Motion_t gyr   = motion.getGyro();
-                gyrMean.x += gyr.x; gyrMean.y += gyr.y; gyrMean.z += gyr.z;
-                wait_us(10000);
-            }
-            gyrMean.x /= 100.0; gyrMean.y /= 100.0; gyrMean.z /= 100.0;
-            isFirstRun = false;
-            
-            //Terminal header
-            printf("%8s,\t%8s,\t%8s,\t%8s,\t%8s,\t%8s,\t%8s,\t%8s,\t%8s,\t%8s,\t%8s\n\r", "acc_x","acc_y","acc_z","gyr_x","gyr_y","gyr_z","temp", "intX", "intY", "intZ", "T");
-            disp.cls();
-
-            continue;
-        }
-
-        //Environmental sensor
-        led1 = 1; led2 = 1; led3 = 1;
-
-        //MEMS
-        led1 = 0; led2 = 0; led3 = 0;  
- 
         Motion_t acc   = motion.getAcceleration();   
-        Motion_t gyr   = motion.getGyro();          gyr.x -= gyrMean.x; gyr.y -= gyrMean.y; gyr.z -= gyrMean.z; 
-        
-        //Measure sampling time
-        now = tmr.elapsed_time();
-        double deltaT = (double)1.0e-6*(now-prev).count();
-        prev = now;        
-
+        Motion_t gyr   = motion.getGyro();          
+            
         //Temperature of sensor
         float tempMems = motion.getTemperatureC();  
 
         //Display sensor values
         printf("%8.3f,\t%8.3f,\t%8.3f,\t", acc.x, acc.y, acc.z); 
         printf("%8.3f,\t%8.3f,\t%8.3f,\t", gyr.x, gyr.y, gyr.z);         
-        printf("%8.3f,\t",             tempMems); 
+        printf("%8.3f,\t\n",           tempMems); 
     
-        
-        //Calculate accelerometer angles
-        double angle_y = alpha * atan( acc.y / sqrt(acc.x*acc.x + acc.z*acc.z) );
-        double angle_x = alpha * atan( acc.x / sqrt(acc.y*acc.y + acc.z*acc.z) );
-        double angle_z = alpha * atan( sqrt( acc.x*acc.x + acc.y*acc.y) / acc.z  );
-        disp.cls();
-        disp.locate(0,0);
-        disp.printf("X:%4.1f", angle_x);    
-        disp.locate(0, 8);
-        disp.printf("Y:%4.1f", angle_y);
-        disp.locate(1, 0);
-        disp.printf("Z:%4.1f", angle_z);        
-
-        // Integrate the Gyro Values
-        gyrAngle.x += deltaT * (gyr.x + gyrPrev.x) * 0.5;
-        gyrAngle.y += deltaT * (gyr.y + gyrPrev.y) * 0.5;
-        gyrAngle.z += deltaT * (gyr.z + gyrPrev.z) * 0.5;
-        gyrPrev = gyr;
-
-        printf("%8.3f,\t%8.3f,\t%8.3f,\t%8.6f\n\r", gyrAngle.x, gyrAngle.y, gyrAngle.z, deltaT);
-
-        //Option to reset the angle
-        if (BlueButton == 1) {
-            buzz.playTone("A");
-            wait_us(500000);
-            gyrAngle = {0.0, 0.0, 0.0};
-            buzz.rest();
-        };
-
         //Loop time is influenced by the following
         wait_us(25000); 
     }
